04221bc2518a4753d750470fe2170fcc45803cb0
Thomas Denewiler <tdenewiler@gmail.com>
2018-08-31 14:26:28 -0700

    v0.1.4

--- setup.py
+++ setup.py
@@ -8,12 +8,20 @@ except:  # pylint: disable=bare-except
 
 import statick_tool
 
+with open('README.md') as f:
+    long_description = f.read()  # pylint: disable=invalid-name
+
 setup(
+    author='SSC Pacific',
     name='statick',
+    description='Tool for running static analysis tools against packages of code.',
     version=statick_tool.__version__,
     packages=['statick_tool'],
     package_data={'statick_tool': ['rsc/*', 'rsc/.clang-format',
                                    'plugins/*.py', 'plugins/discovery/*',
                                    'plugins/tool/*']},
-    scripts=['statick', 'statick_ws', 'statick_gauntlet']
+    scripts=['statick', 'statick_ws', 'statick_gauntlet'],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/sscpac/statick'
 )
--- statick_tool/__init__.py
+++ statick_tool/__init__.py
@@ -1,3 +1,3 @@
 """Tool for running static analysis tools against packages of code."""
 
-__version__ = "0.1.3"
+__version__ = "0.1.4"
