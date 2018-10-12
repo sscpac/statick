9f2316682b6682a051cfb262afb17fe0b4eb2752
Chris Reffett <chris.reffett@soartech.com>
2018-10-12 15:51:07 -0400

    Add c discovery tests

---
+++ tests/plugins/discovery/c_discovery_plugin/test_c_discovery_plugin.py
@@ -0,0 +1,48 @@
+import os
+import statick_tool
+
+from statick_tool.discovery_plugin import DiscoveryPlugin
+from statick_tool.package import Package
+from statick_tool.plugins.discovery.c_discovery_plugin \
+    import CDiscoveryPlugin
+from yapsy.PluginManager import PluginManager
+
+
+def test_c_discovery_plugin_found():
+    manager = PluginManager()
+    # Get the path to statick_tool/__init__.py, get the directory part, and
+    # add 'plugins' to that to get the standard plugins dir
+    manager.setPluginPlaces([os.path.join(os.path.dirname(statick_tool.__file__),
+                                          'plugins')])
+    manager.setCategoriesFilter({
+        "Discovery": DiscoveryPlugin,
+    })
+    manager.collectPlugins()
+    # Verify that a plugin's get_name() function returns "c"
+    assert(any(plugin_info.plugin_object.get_name() == 'C' for
+               plugin_info in manager.getPluginsOfCategory("Discovery")))
+    # While we're at it, verify that a plugin is named C Discovery Plugin
+    assert(any(plugin_info.name == 'C/C++ Discovery Plugin' for
+               plugin_info in manager.getPluginsOfCategory("Discovery")))
+
+def test_c_discovery_plugin_scan_valid():
+    cdp = CDiscoveryPlugin()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    cdp.scan(package, 'level')
+    expected = [ 'test.c', 'test.cpp', 'test.cc', 'test.cxx', 'test.h',
+                 'test.hxx', 'test.hpp', 'oddextensioncpp.source',
+                 'oddextensionc.source']
+    # We have to add the path to each of the above...yuck
+    expected_fullpath = [ os.path.join(package.path, filename)
+                          for filename in expected ]
+    # Neat trick to verify that two unordered lists are the same
+    assert(set(package['c_src']) == set(expected_fullpath))
+
+def test_c_discovery_plugin_scan_invalid_nocmake():
+    cdp = CDiscoveryPlugin()
+    package = Package('invalid_package',
+                      os.path.join(os.path.dirname(__file__),
+                                   'invalid_package'))
+    cdp.scan(package, 'level')
+    assert(not package['c_src'])
