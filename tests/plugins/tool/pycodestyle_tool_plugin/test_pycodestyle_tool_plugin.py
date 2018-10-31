eb6e346486b9293a41c28fcd42b4bb35886bfffe
Chris Reffett <chris.reffett@soartech.com>
2018-10-31 14:42:32 -0400

    Rename pep8/pep257 tests

--- tests/plugins/tool/pycodestyle_tool_plugin/test_pycodestyle_tool_plugin.py
+++ tests/plugins/tool/pycodestyle_tool_plugin/test_pycodestyle_tool_plugin.py
@@ -7,12 +7,13 @@ import statick_tool
 from statick_tool.config import Config
 from statick_tool.package import Package
 from statick_tool.plugin_context import PluginContext
-from statick_tool.plugins.tool.pep8_tool_plugin import Pep8ToolPlugin
+from statick_tool.plugins.tool.pycodestyle_tool_plugin import \
+    PycodestyleToolPlugin
 from statick_tool.resources import Resources
 from statick_tool.tool_plugin import ToolPlugin
 
 
-def setup_pep8_tool_plugin():
+def setup_pycodestyle_tool_plugin():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--show-tool-output", dest="show_tool_output",
                             action="store_true", help="Show tool output")
@@ -21,12 +22,12 @@ def setup_pep8_tool_plugin():
                            'plugins')])
     config = Config(resources.get_file("config.yaml"))
     plugin_context = PluginContext(arg_parser.parse_args([]), resources, config)
-    p8tp = Pep8ToolPlugin()
-    p8tp.set_plugin_context(plugin_context)
-    return p8tp
+    pcstp = PycodestyleToolPlugin()
+    pcstp.set_plugin_context(plugin_context)
+    return pcstp
 
 
-def test_pep8_tool_plugin_found():
+def test_pycodestyle_tool_plugin_found():
     manager = PluginManager()
     # Get the path to statick_tool/__init__.py, get the directory part, and
     # add 'plugins' to that to get the standard plugins dir
@@ -36,42 +37,42 @@ def test_pep8_tool_plugin_found():
         "Tool": ToolPlugin,
     })
     manager.collectPlugins()
-    # Verify that a plugin's get_name() function returns "pep8"
-    assert(any(plugin_info.plugin_object.get_name() == 'pep8' for
+    # Verify that a plugin's get_name() function returns "pycodestyle"
+    assert(any(plugin_info.plugin_object.get_name() == 'pycodestyle' for
                plugin_info in manager.getPluginsOfCategory("Tool")))
-    # While we're at it, verify that a plugin is named Pep8 Tool Plugin
-    assert(any(plugin_info.name == 'PEP8 Tool Plugin' for
+    # While we're at it, verify that a plugin is named Pycodestyle Tool Plugin
+    assert(any(plugin_info.name == 'Pycodestyle Tool Plugin' for
                plugin_info in manager.getPluginsOfCategory("Tool")))
 
 
-def test_pep8_tool_plugin_scan_valid():
-    '''Integration test: Make sure the pep8 output hasn't changed'''
-    p8tp = setup_pep8_tool_plugin()
+def test_pycodestyle_tool_plugin_scan_valid():
+    '''Integration test: Make sure the pycodestyle output hasn't changed'''
+    pcstp = setup_pycodestyle_tool_plugin()
     package = Package('valid_package', os.path.join(os.path.dirname(__file__),
                                                     'valid_package'))
     package['python_src'] = [os.path.join(os.path.dirname(__file__),
                                           'valid_package', 'e501.py')]
-    issues = p8tp.scan(package, 'level')
+    issues = pcstp.scan(package, 'level')
     assert(len(issues) == 1)
 
 
-def test_pep8_tool_plugin_parse_valid():
-    '''Verify that we can parse the normal output of pep8'''
-    p8tp = setup_pep8_tool_plugin()
+def test_pycodestyle_tool_plugin_parse_valid():
+    '''Verify that we can parse the normal output of pycodestyle'''
+    pcstp = setup_pycodestyle_tool_plugin()
     output = "valid_package/e501.py:1: [E501] line too long (88 > 79 characters)"
-    issues = p8tp.parse_output([output])
+    issues = pcstp.parse_output([output])
     assert(len(issues) == 1)
     assert(issues[0].filename == 'valid_package/e501.py')
     assert(issues[0].line_number == '1')
-    assert(issues[0].tool == 'pep8')
+    assert(issues[0].tool == 'pycodestyle')
     assert(issues[0].issue_type == 'E501')
     assert(issues[0].severity == '5')
     assert(issues[0].message == "line too long (88 > 79 characters)")
 
 
-def test_pep8_tool_plugin_parse_invalid():
-    '''Verify that we can parse the normal output of pep8'''
-    p8tp = setup_pep8_tool_plugin()
+def test_pycodestyle_tool_plugin_parse_invalid():
+    '''Verify that we can parse the normal output of pycodestyle'''
+    pcstp = setup_pycodestyle_tool_plugin()
     output = "invalid text"
-    issues = p8tp.parse_output(output)
+    issues = pcstp.parse_output(output)
     assert(not issues)
--- tests/plugins/tool/pydocstyle_tool_plugin/test_pydocstyle_tool_plugin.py
+++ tests/plugins/tool/pydocstyle_tool_plugin/test_pydocstyle_tool_plugin.py
@@ -7,12 +7,13 @@ import statick_tool
 from statick_tool.config import Config
 from statick_tool.package import Package
 from statick_tool.plugin_context import PluginContext
-from statick_tool.plugins.tool.pep257_tool_plugin import Pep257ToolPlugin
+from statick_tool.plugins.tool.pydocstyle_tool_plugin import \
+    PydocstyleToolPlugin
 from statick_tool.resources import Resources
 from statick_tool.tool_plugin import ToolPlugin
 
 
-def setup_pep257_tool_plugin():
+def setup_pydocstyle_tool_plugin():
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--show-tool-output", dest="show_tool_output",
                             action="store_true", help="Show tool output")
@@ -21,12 +22,12 @@ def setup_pep257_tool_plugin():
                            'plugins')])
     config = Config(resources.get_file("config.yaml"))
     plugin_context = PluginContext(arg_parser.parse_args([]), resources, config)
-    p257tp = Pep257ToolPlugin()
-    p257tp.set_plugin_context(plugin_context)
-    return p257tp
+    pdstp = PydocstyleToolPlugin()
+    pdstp.set_plugin_context(plugin_context)
+    return pdstp
 
 
-def test_pep257_tool_plugin_found():
+def test_pydocstyle_tool_plugin_found():
     manager = PluginManager()
     # Get the path to statick_tool/__init__.py, get the directory part, and
     # add 'plugins' to that to get the standard plugins dir
@@ -36,43 +37,43 @@ def test_pep257_tool_plugin_found():
         "Tool": ToolPlugin,
     })
     manager.collectPlugins()
-    # Verify that a plugin's get_name() function returns "pep257"
-    assert(any(plugin_info.plugin_object.get_name() == 'pep257' for
+    # Verify that a plugin's get_name() function returns "pydocstyle"
+    assert(any(plugin_info.plugin_object.get_name() == 'pydocstyle' for
                plugin_info in manager.getPluginsOfCategory("Tool")))
-    # While we're at it, verify that a plugin is named Pep257 Tool Plugin
-    assert(any(plugin_info.name == 'PEP257 Tool Plugin' for
+    # While we're at it, verify that a plugin is named Pydocstyle Tool Plugin
+    assert(any(plugin_info.name == 'Pydocstyle Tool Plugin' for
                plugin_info in manager.getPluginsOfCategory("Tool")))
 
 
-def test_pep257_tool_plugin_scan_valid():
-    '''Integration test: Make sure the pep257 output hasn't changed'''
-    p257tp = setup_pep257_tool_plugin()
+def test_pydocstyle_tool_plugin_scan_valid():
+    '''Integration test: Make sure the pydocstyle output hasn't changed'''
+    pdstp = setup_pydocstyle_tool_plugin()
     package = Package('valid_package', os.path.join(os.path.dirname(__file__),
                                                     'valid_package'))
     package['python_src'] = [os.path.join(os.path.dirname(__file__),
                              'valid_package', 'd103.py')]
-    issues = p257tp.scan(package, 'level')
+    issues = pdstp.scan(package, 'level')
     assert(len(issues) == 1)
 
 
-def test_pep257_tool_plugin_parse_valid():
-    '''Verify that we can parse the normal output of pep257'''
-    p257tp = setup_pep257_tool_plugin()
+def test_pydocstyle_tool_plugin_parse_valid():
+    '''Verify that we can parse the normal output of pydocstyle'''
+    pdstp = setup_pydocstyle_tool_plugin()
     output = "valid_package/d103.py:3 in public function `some_method`:\n\
  D103: Missing docstring in public function"
-    issues = p257tp.parse_output([output])
+    issues = pdstp.parse_output([output])
     assert(len(issues) == 1)
     assert(issues[0].filename == 'valid_package/d103.py')
     assert(issues[0].line_number == '3')
-    assert(issues[0].tool == 'pep257')
+    assert(issues[0].tool == 'pydocstyle')
     assert(issues[0].issue_type == 'D103')
     assert(issues[0].severity == '5')
     assert(issues[0].message == "Missing docstring in public function")
 
 
-def test_pep257_tool_plugin_parse_invalid():
-    '''Verify that we can parse the normal output of pep257'''
-    p257tp = setup_pep257_tool_plugin()
+def test_pydocstyle_tool_plugin_parse_invalid():
+    '''Verify that we can parse the normal output of pydocstyle'''
+    pdstp = setup_pydocstyle_tool_plugin()
     output = "invalid text"
-    issues = p257tp.parse_output(output)
+    issues = pdstp.parse_output(output)
     assert(not issues)
