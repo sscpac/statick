51d0199497e57f5f4ecdf35ab374d0fe6738f1a4
Chris Reffett <chris.reffett@soartech.com>
2019-02-05 11:05:26 -0500

    Add Maven discovery plugin

---
+++ statick_tool/plugins/discovery/maven_discovery_plugin.py
@@ -0,0 +1,43 @@
+"""Discover Maven POM files to analyze."""
+
+from __future__ import print_function
+
+import fnmatch
+import os
+from collections import OrderedDict
+
+from statick_tool.discovery_plugin import DiscoveryPlugin
+
+
+class MavenDiscoveryPlugin(DiscoveryPlugin):
+    """Discover Maven files to analyze."""
+
+    def get_name(self):
+        """Get name of discovery type."""
+        return "maven"
+
+    def scan(self, package, level):
+        """Scan package looking for maven files."""
+        top_poms = []
+        all_poms = []
+        deepest_pom_level = 999999
+
+        for root, _, files in os.walk(package.path):
+            for f in fnmatch.filter(files, "pom.xml"):
+                full_path = os.path.join(root, f)
+                depth = full_path.count(os.sep)
+                if depth < deepest_pom_level:
+                    deepest_pom_level = depth
+                    top_poms = []
+                if depth == deepest_pom_level:
+                    top_poms.append(full_path)
+                all_poms.append(full_path)
+
+        top_poms = list(OrderedDict.fromkeys(top_poms))
+        all_poms = list(OrderedDict.fromkeys(all_poms))
+
+        print("  {} Maven POM files found.".format(len(all_poms)))
+        print("  {} top-level Maven POM files found.".format(len(top_poms)))
+
+        package["all_poms"] = all_poms
+        package["top_poms"] = top_poms
