3aa1e593bfb871aa1f825e354892325dd32d2fb2
Thomas Denewiler <tdenewiler@gmail.com>
2019-07-22 21:16:54 -0700

    Clang format unit test (#125)
    Increasing test cases and coverage for clang-format tool plugin.

--- tests/plugins/tool/clang-format_tool_plugin/test_clang-format_tool_plugin.py
+++ tests/plugins/tool/clang-format_tool_plugin/test_clang-format_tool_plugin.py
@@ -1,11 +1,15 @@
 """Unit tests for the clang-format plugin."""
 import argparse
 import os
+# import shutil
+import subprocess
 
+import mock
 from yapsy.PluginManager import PluginManager
 
 import statick_tool
 from statick_tool.config import Config
+from statick_tool.package import Package
 from statick_tool.plugin_context import PluginContext
 from statick_tool.plugins.tool.clang_format_tool_plugin import \
     ClangFormatToolPlugin
@@ -17,11 +21,30 @@ def setup_clang_format_tool_plugin():
     """Initialize and return an instance of the clang-format plugin."""
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--show-tool-output", dest="show_tool_output",
-                            action="store_true", help="Show tool output")
+                            action="store_false", help="Show tool output")
     arg_parser.add_argument("--clang-format-bin", dest="clang_format_bin")
     arg_parser.add_argument("--clang-format-raise-exception",
                             dest="clang_format_raise_exception",
-                            action="store_true", default=True)
+                            action="store_false", default=True)
+
+    resources = Resources([os.path.join(os.path.dirname(statick_tool.__file__),
+                                        'plugins')])
+    config = Config(resources.get_file("config.yaml"))
+    plugin_context = PluginContext(arg_parser.parse_args([]), resources, config)
+    cftp = ClangFormatToolPlugin()
+    cftp.set_plugin_context(plugin_context)
+    return cftp
+
+
+def setup_clang_format_tool_plugin_non_default():
+    """Initialize and return an instance of the clang-format plugin."""
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("--show-tool-output", dest="show_tool_output",
+                            action="store_false", help="Show tool output")
+    arg_parser.add_argument("--clang-format-bin", dest="clang_format_bin")
+    arg_parser.add_argument("--clang-format-raise-exception",
+                            dest="clang_format_raise_exception",
+                            action="store_true", default=False)
 
     resources = Resources([os.path.join(os.path.dirname(statick_tool.__file__),
                                         'plugins')])
@@ -46,30 +69,35 @@ def test_clang_format_tool_plugin_found():
     # Verify that a plugin's get_name() function returns "clang_format"
     assert any(plugin_info.plugin_object.get_name() == 'clang-format' for
                plugin_info in manager.getPluginsOfCategory("Tool"))
-    # While we're at it, verify that a plugin is named ClangFormat Tool Plugin
+    # While we're at it, verify that a plugin is named clang-format Tool Plugin
     assert any(plugin_info.name == 'clang-format Tool Plugin' for
                plugin_info in manager.getPluginsOfCategory("Tool"))
 
 
-# Has issues with not finding the clang-format config correctly. Plugin probably could
-# use some touching up in this department
+# Has issues with not finding the clang-format config correctly on Travis CI.
+# Plugin probably could use some touching up in this department.
 # def test_clang_format_tool_plugin_scan_valid():
-#    """Integration test: Make sure the clang_format output hasn't changed."""
-#    cftp = setup_clang_format_tool_plugin()
-#    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
-#                                                    'valid_package'))
+#     """Integration test: Make sure the clang_format output hasn't changed."""
+#     cftp = setup_clang_format_tool_plugin()
+#     package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+#                                                     'valid_package'))
+#     # Issues should be empty until make_targets is added to the package.
+#     issues = cftp.scan(package, 'level')
+#     assert not issues
 #
-#    # Copy the latest clang_format over
-#    shutil.copyfile(cftp.plugin_context.resources.get_file("_clang-format"),
-#                    os.path.join(os.path.dirname(__file__), '_clang-format'))
-#    package['make_targets'] = []
-#    package['make_targets'].append({})
-#    package['make_targets'][0]['src'] = [os.path.join(os.path.dirname(__file__),
-#                                                      'valid_package', 'indents.c')]
-#    issues = cftp.scan(package, 'level')
-#    assert len(issues) == 1
-#
-#
+#     # Copy the latest clang_format over
+#     shutil.copyfile(cftp.plugin_context.resources.get_file("_clang-format"),
+#                     os.path.join(os.path.dirname(__file__), '_clang-format'))
+#     package['make_targets'] = []
+#     package['make_targets'].append({})
+#     package['make_targets'][0]['src'] = [os.path.join(os.path.dirname(__file__),
+#                                                       'valid_package', 'indents.c')]
+#     package['headers'] = [os.path.join(os.path.dirname(__file__),
+#                                        'valid_package', 'indents.h')]
+#     issues = cftp.scan(package, 'level')
+#     assert len(issues) == 1
+
+
 def test_clang_format_tool_plugin_parse_valid():
     """Verify that we can parse the normal output of clang_format."""
     cftp = setup_clang_format_tool_plugin()
@@ -94,3 +122,54 @@ def test_clang_format_tool_plugin_parse_invalid():
     output = "invalid text"
     issues = cftp.parse_output(output)
     assert not issues
+
+
+@mock.patch('statick_tool.plugins.tool.clang_format_tool_plugin.subprocess.check_output')
+def test_clang_format_tool_plugin_scan_calledprocesserror(mock_subprocess_check_output):
+    """
+    Test what happens when a CalledProcessError is raised (usually means clang-format hit an error).
+
+    Expected result: issues is empty
+    """
+    mock_subprocess_check_output.side_effect = subprocess.CalledProcessError(1, '', output="mocked error")
+    cftp = setup_clang_format_tool_plugin()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['make_targets'] = []
+    package['headers'] = []
+    issues = cftp.scan(package, 'level')
+    assert not issues
+
+
+@mock.patch('statick_tool.plugins.tool.clang_format_tool_plugin.subprocess.check_output')
+def test_clang_format_tool_plugin_scan_calledprocesserror_non_default(mock_subprocess_check_output):
+    """
+    Test what happens when a CalledProcessError is raised (usually means clang-format hit an error).
+
+    Expected result: issues is empty
+    """
+    mock_subprocess_check_output.side_effect = subprocess.CalledProcessError(1, '', output="mocked error")
+    cftp = setup_clang_format_tool_plugin_non_default()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['make_targets'] = []
+    package['headers'] = []
+    issues = cftp.scan(package, 'level')
+    assert not issues
+
+
+@mock.patch('statick_tool.plugins.tool.clang_format_tool_plugin.subprocess.check_output')
+def test_clang_format_tool_plugin_scan_oserror(mock_subprocess_check_output):
+    """
+    Test what happens when an OSError is raised (usually means clang-format doesn't exist).
+
+    Expected result: issues is None
+    """
+    mock_subprocess_check_output.side_effect = OSError('mocked error')
+    cftp = setup_clang_format_tool_plugin()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['make_targets'] = []
+    package['headers'] = []
+    issues = cftp.scan(package, 'level')
+    assert issues is None
---
+++ tests/plugins/tool/clang-format_tool_plugin/valid_package/indents.h
@@ -0,0 +1,3 @@
+#ifndef VALID_PACKAGE_INDENTS_H
+#define VALID_PACKAGE_INDENTS_H
+#endif
