ad7d31ad3b5e029baa7cf9b70f291c758d9ef2cd
Thomas Denewiler <tdenewiler@gmail.com>
2019-08-02 18:03:06 -0700

    Adding unit tests for lizard tool plugin.

--- tests/plugins/tool/clang-tidy_tool_plugin/test_clang-tidy_tool_plugin.py
+++ tests/plugins/tool/clang-tidy_tool_plugin/test_clang-tidy_tool_plugin.py
@@ -148,7 +148,7 @@ def test_clang_tidy_tool_plugin_parse_invalid():
     assert not issues
 
 
-def test_bandit_tool_plugin_scan_missing_fields():
+def test_clang_tidy_tool_plugin_scan_missing_fields():
     """
     Test what happens when key fields are missing from the Package argument.
 
@@ -168,7 +168,7 @@ def test_bandit_tool_plugin_scan_missing_fields():
 
 
 @mock.patch('statick_tool.plugins.tool.clang_tidy_tool_plugin.subprocess.check_output')
-def test_bandit_tool_plugin_scan_oserror(mock_subprocess_check_output):
+def test_clang_tidy_tool_plugin_scan_oserror(mock_subprocess_check_output):
     """
     Test what happens when an OSError is raised (usually means clang-tidy doesn't exist).
 
---
+++ tests/plugins/tool/lizard_tool_plugin/valid_package/test.c
@@ -0,0 +1,70 @@
+#include <stdlib.h>
+void func()
+{
+  int a, b, c, d, e, f, g, h, i, j, k, l, m, n, o;
+
+  if (a)
+  {
+    // Do nothing.
+  }
+  else if (b)
+  {
+    // Do nothing.
+  }
+  else if (c)
+  {
+    // Do nothing.
+  }
+  else if (d)
+  {
+    // Do nothing.
+  }
+  else if (e)
+  {
+    // Do nothing.
+  }
+  else if (f)
+  {
+    // Do nothing.
+  }
+  else if (g)
+  {
+    // Do nothing.
+  }
+  else if (h)
+  {
+    // Do nothing.
+  }
+  else if (i)
+  {
+    // Do nothing.
+  }
+  else if (j)
+  {
+    // Do nothing.
+  }
+  else if (k)
+  {
+    // Do nothing.
+  }
+  else if (l)
+  {
+    // Do nothing.
+  }
+  else if (m)
+  {
+    // Do nothing.
+  }
+  else if (n)
+  {
+    // Do nothing.
+  }
+  else if (o)
+  {
+    // Do nothing.
+  }
+  else
+  {
+    // Do nothing.
+  }
+}
