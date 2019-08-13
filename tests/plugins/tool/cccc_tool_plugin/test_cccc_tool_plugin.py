bc13f560662710801f69d2425db44f395f758c41
Thomas Denewiler <tdenewiler@gmail.com>
2019-08-12 21:36:48 -0700

    Increasing test cases and coverage for CCCC tool plugin.

--- tests/plugins/tool/cccc_tool_plugin/test_cccc_tool_plugin.py
+++ tests/plugins/tool/cccc_tool_plugin/test_cccc_tool_plugin.py
@@ -5,7 +5,9 @@ from __future__ import print_function
 import argparse
 import os
 import shutil
+import subprocess
 
+import mock
 import xmltodict
 from yapsy.PluginManager import PluginManager
 
@@ -58,12 +60,24 @@ def test_cccc_tool_plugin_found():
 def test_cccc_tool_plugin_scan_valid():
     """Integration test: Make sure the CCCC output hasn't changed."""
     ctp = setup_cccc_tool_plugin()
+
     package = Package('valid_package', os.path.join(os.path.dirname(__file__),
                                                     'valid_package'))
     package['c_src'] = [os.path.join(os.path.dirname(__file__),
                                      'valid_package', 'example.cpp')]
     issues = ctp.scan(package, 'level')
     print('issues: {}'.format(issues))
+    assert not issues
+
+
+def test_cccc_tool_plugin_scan_missing_field():
+    """Check that a missing set of source files results in empty issues."""
+    ctp = setup_cccc_tool_plugin()
+
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    # Missing package["c_src"]
+    issues = ctp.scan(package, 'level')
     assert not issues
 
 
@@ -86,13 +100,48 @@ def test_cccc_tool_plugin_parse_valid():
 
     issues = ctp.parse_output(output, package, config_file)
     print('issues: {}'.format(issues))
-    assert len(issues) == 1
+    assert len(issues) == 2
     assert issues[0].filename == 'tmp/not_a_file.c'
     assert issues[0].line_number == 0
     assert issues[0].tool == 'cccc'
-    assert issues[0].issue_type == 'warn'
-    assert issues[0].severity == 3
-    assert issues[0].message == 'Example1 - Fan in (concrete uses only) - value: 7.0, theshold: 6.0'
+
+    if issues[0].severity == 5:
+        assert issues[0].issue_type == 'error'
+        assert issues[0].severity == 5
+        assert issues[0].message == 'Example1 - Henry-Kafura/Shepperd measure (overall) - value: 10000.0, theshold: 1000.0'
+
+        assert issues[1].issue_type == 'warn'
+        assert issues[1].severity == 3
+        assert issues[1].message == 'Example1 - Fan in (concrete uses only) - value: 7.0, theshold: 6.0'
+    else:
+        assert issues[0].issue_type == 'warn'
+        assert issues[0].severity == 3
+        assert issues[0].message == 'Example1 - Fan in (concrete uses only) - value: 7.0, theshold: 6.0'
+        assert issues[1].issue_type == 'error'
+        assert issues[1].severity == 5
+        assert issues[1].message == 'Example1 - Henry-Kafura/Shepperd measure (overall) - value: 10000.0, theshold: 1000.0'
+
+
+def test_cccc_tool_plugin_parse_missing_names():
+    """Verify that we can parse the output of CCCC when fields are missing."""
+    ctp = setup_cccc_tool_plugin()
+
+    # Copy the latest configuration file over.
+    shutil.copyfile(ctp.plugin_context.resources.get_file("cccc.opt"),
+                    os.path.join(os.path.dirname(__file__), 'cccc.opt'))
+    config_file = ctp.plugin_context.resources.get_file('cccc.opt')
+
+    output_file = os.path.join(os.path.dirname(__file__), 'valid_package', 'cccc-missing-names.xml')
+    with open(output_file) as f:
+        output = xmltodict.parse(f.read())
+
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['c_src'] = ['tmp/not_a_file.c']
+
+    issues = ctp.parse_output(output, package, config_file)
+    print('issues: {}'.format(issues))
+    assert not issues
 
 
 def test_cccc_tool_plugin_parse_invalid():
@@ -101,7 +150,53 @@ def test_cccc_tool_plugin_parse_invalid():
     shutil.copyfile(ctp.plugin_context.resources.get_file("cccc.opt"),
                     os.path.join(os.path.dirname(__file__), 'cccc.opt'))
     config_file = ctp.plugin_context.resources.get_file('cccc.opt')
+
     output = "invalid text"
     package = {"c_src": "/tmp/not_a_file.c"}
     issues = ctp.parse_output(output, package, config_file)
+    assert not issues
+
+
+def test_cccc_tool_plugin_parse_config_none():
+    """Verify that we get empty config if config file is None."""
+    ctp = setup_cccc_tool_plugin()
+    config = ctp.parse_config(None)
+    assert not config
+
+
+@mock.patch('statick_tool.plugins.tool.cccc_tool_plugin.subprocess.check_output')
+def test_cccc_tool_plugin_scan_empty_oserror(mock_subprocess_check_output):
+    """
+    Test what happens an OSError is hit (such as if cccc doesn't exist)
+
+    Expected result: issues is an empty list
+    """
+    mock_subprocess_check_output.side_effect = OSError('mocked error')
+    ctp = setup_cccc_tool_plugin()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['c_src'] = [os.path.join(os.path.dirname(__file__),
+                                     'valid_package', 'example.cpp')]
+    issues = ctp.scan(package, 'level')
+    assert issues is None
+
+
+@mock.patch('statick_tool.plugins.tool.cccc_tool_plugin.subprocess.check_output')
+def test_cccc_tool_plugin_scan_empty_calledprocesserror(mock_subprocess_check_output):
+    """
+    Test what happens when a CalledProcessError is hit (such as if cccc encounters an error).
+
+    Expected result: issues is an empty list
+    """
+    mock_subprocess_check_output.side_effect = subprocess.CalledProcessError(0, '', output=b"mocked error")
+    ctp = setup_cccc_tool_plugin()
+    package = Package('valid_package', os.path.join(os.path.dirname(__file__),
+                                                    'valid_package'))
+    package['c_src'] = [os.path.join(os.path.dirname(__file__),
+                                     'valid_package', 'example.cpp')]
+    issues = ctp.scan(package, 'level')
+    assert issues is None
+
+    mock_subprocess_check_output.side_effect = subprocess.CalledProcessError(1, '', output=b"mocked error")
+    issues = ctp.scan(package, 'level')
     assert not issues
