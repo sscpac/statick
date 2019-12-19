3a144b0f32bda383ad735a5a9a8ff8bce3c6c69f
Thomas Denewiler <tdenewiler@gmail.com>
2019-12-19 15:58:50 -0800

    Adding unit tests to make sure expected results are obtained when the… (#162)
    Adding unit tests to make sure expected results are obtained when the file command does not exist.

--- tests/plugins/discovery/c_discovery_plugin/test_c_discovery_plugin.py
+++ tests/plugins/discovery/c_discovery_plugin/test_c_discovery_plugin.py
@@ -1,4 +1,5 @@
 """Unit tests for the C discovery plugin."""
+import contextlib
 import os
 
 from yapsy.PluginManager import PluginManager
@@ -8,6 +9,37 @@ from statick_tool.discovery_plugin import DiscoveryPlugin
 from statick_tool.exceptions import Exceptions
 from statick_tool.package import Package
 from statick_tool.plugins.discovery.c_discovery_plugin import CDiscoveryPlugin
+
+
+# From https://stackoverflow.com/questions/2059482/python-temporarily-modify-the-current-processs-environment
+@contextlib.contextmanager
+def modified_environ(*remove, **update):
+    """
+    Temporarily updates the ``os.environ`` dictionary in-place.
+
+    The ``os.environ`` dictionary is updated in-place so that the modification
+    is sure to work in all situations.
+    :param remove: Environment variables to remove.
+    :param update: Dictionary of environment variables and values to add/update.
+    """
+    env = os.environ
+    update = update or {}
+    remove = remove or []
+
+    # List of environment variables being updated or removed.
+    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
+    # Environment variables and values to restore on exit.
+    update_after = {k: env[k] for k in stomped}
+    # Environment variables and values to remove on exit.
+    remove_after = frozenset(k for k in update if k not in env)
+
+    try:
+        env.update(update)
+        [env.pop(k, None) for k in remove]
+        yield
+    finally:
+        env.update(update_after)
+        [env.pop(k) for k in remove_after]
 
 
 def test_c_discovery_plugin_found():
@@ -72,3 +104,25 @@ def test_c_discovery_plugin_scan_exceptions():
                          for filename in expected]
     # Neat trick to verify that two unordered lists are the same
     assert set(package['c_src']) == set(expected_fullpath)
+
+
+def test_c_discovery_plugin_no_file_cmd():
+    """
+    Test when file command does not exist.
+
+    Test that files are not discovered with file command output if file
+    command does not exist.
+    """
+    with modified_environ(PATH=''):
+        cdp = CDiscoveryPlugin()
+        package = Package('valid_package',
+                          os.path.join(os.path.dirname(__file__),
+                                       'valid_package'))
+        cdp.scan(package, 'level')
+        expected = ['test.c', 'test.cpp', 'test.cc', 'test.cxx', 'test.h',
+                    'test.hxx', 'test.hpp', os.path.join('ignore_this', 'ignoreme.c')]
+        # We have to add the path to each of the above...yuck
+        expected_fullpath = [os.path.join(package.path, filename)
+                             for filename in expected]
+        # Neat trick to verify that two unordered lists are the same
+        assert set(package['c_src']) == set(expected_fullpath)
