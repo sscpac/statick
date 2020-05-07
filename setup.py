dc9eb0502b068132f2b98f123b5a9a3a0d4e62ed
Thomas Denewiler <tdenewiler@gmail.com>
2020-05-07 09:23:49 -0700

    Remove legacy Jenkins reporting plugin (#234)

--- setup.py
+++ setup.py
@@ -8,44 +8,59 @@ except:  # pylint: disable=bare-except # noqa: E722 # NOLINT
 
 import statick_tool
 
-with open('README.md') as f:
+with open("README.md") as f:
     LONG_DESCRIPTION = f.read()
 
 TEST_DEPS = [
-    'backports.tempfile',
-    'pylint-django',
-    'pytest',
-    'mock',
-    'tox',
+    "backports.tempfile",
+    "pylint-django",
+    "pytest",
+    "mock",
+    "tox",
 ]
 
 EXTRAS = {
-    'test': TEST_DEPS,
+    "test": TEST_DEPS,
 }
 
 setup(
-    name='statick',
-    description='Making code quality easier.',
-    author='SSC Pacific',
+    name="statick",
+    description="Making code quality easier.",
+    author="SSC Pacific",
     version=statick_tool.__version__,
-    packages=['statick_tool'],
-    package_data={'statick_tool': ['rsc/.*',
-                                   'rsc/*',
-                                   'rsc/plugin_mapping/*',
-                                   'plugins/*.py',
-                                   'plugins/discovery/*',
-                                   'plugins/tool/*',
-                                   'plugins/reporting/*']},
-    scripts=['statick', 'statick_ws'],
+    packages=["statick_tool"],
+    package_data={
+        "statick_tool": [
+            "rsc/.*",
+            "rsc/*",
+            "rsc/plugin_mapping/*",
+            "plugins/*.py",
+            "plugins/discovery/*",
+            "plugins/tool/*",
+            "plugins/reporting/*",
+        ]
+    },
+    scripts=["statick", "statick_ws"],
     long_description=LONG_DESCRIPTION,
-    long_description_content_type='text/markdown',
-    install_requires=['bandit', 'cmakelint', 'cpplint', 'deprecated',
-                      'flawfinder', 'lizard', 'pycodestyle', 'pydocstyle',
-                      'pyflakes', 'pylint', 'PyYAML', 'xmltodict', 'yamllint',
-                      'yapsy'],
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "bandit",
+        "cmakelint",
+        "cpplint",
+        "flawfinder",
+        "lizard",
+        "pycodestyle",
+        "pydocstyle",
+        "pyflakes",
+        "pylint",
+        "PyYAML",
+        "xmltodict",
+        "yamllint",
+        "yapsy",
+    ],
     tests_require=TEST_DEPS,
     extras_require=EXTRAS,
-    url='https://github.com/sscpac/statick',
+    url="https://github.com/sscpac/statick",
     classifiers=[
         "License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication",
         "Programming Language :: Python :: 3",
