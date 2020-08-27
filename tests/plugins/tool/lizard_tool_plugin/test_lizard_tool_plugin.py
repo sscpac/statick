d4abe47051ce93e734cbd30f638b3a8e8a9470be
Thomas Denewiler <tdenewiler@gmail.com>
2020-08-27 10:50:43 -0700

    black

--- tests/plugins/tool/lizard_tool_plugin/test_lizard_tool_plugin.py
+++ tests/plugins/tool/lizard_tool_plugin/test_lizard_tool_plugin.py
@@ -90,8 +90,10 @@ def test_lizard_tool_plugin_scan_valid():
 def test_lizard_tool_plugin_parse_valid():
     """Verify that we can parse the normal output of lizard."""
     ltp = setup_lizard_tool_plugin()
-    output = "{}:1: warning: func has 22 NLOC, 18 CCN, 143 token, 0 PARAM, 69 length".format(
-        os.path.join("valid_package", "test.c")
+    output = (
+        "{}:1: warning: func has 22 NLOC, 18 CCN, 143 token, 0 PARAM, 69 length".format(
+            os.path.join("valid_package", "test.c")
+        )
     )
     issues = ltp.parse_output(output)
     assert len(issues) == 1
