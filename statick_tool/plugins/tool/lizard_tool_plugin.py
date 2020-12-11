e35dd7fc5401b1274d52a2edcf816b9784655c7b
Jacob Hassold <jhassold@dcscorp.com>
2020-12-11 13:06:27 -0500

    Trying to get user flags working Distro A, OPSEC #4584
    Signed-off-by: Jacob Hassold <jhassold@dcscorp.com>

--- statick_tool/plugins/tool/lizard_tool_plugin.py
+++ statick_tool/plugins/tool/lizard_tool_plugin.py
@@ -1,4 +1,5 @@
 """Apply lizard tool and gather results."""
+import json
 import re
 import subprocess
 import sys
@@ -25,10 +26,12 @@ class LizardToolPlugin(ToolPlugin):
 
         try:
             ## The following is basically copy-pasted from lizard.py's main() ##
-            user_flags = self.get_user_flags(level)
+            user_flags = [lizard.__file__] + self.get_user_flags(level) # leading None is required
             if '-w' not in user_flags:
                 user_flags += ['-w']
+            print('user_flags: {}'.format(user_flags))
             options = lizard.parse_args(user_flags)
+            # print(options)
             printer = options.printer or lizard.print_result
             schema = lizard.OutputScheme(options.extensions)
             if schema.any_silent():
@@ -41,20 +44,30 @@ class LizardToolPlugin(ToolPlugin):
             if options.output_file:
                 output_file = lizard.open_output_file(options.output_file)
                 sys.stdout = output_file
+            print("\nLizard options: {}\n".format(options))
+            # print('Lizard paths: {}'.format(options.paths))
+            # print('Lizard directory: {}'.format(src_dir))
+            print('options.printer: {}'.format(options.printer))
+            print('options.paths: {}'.format(options.paths))
+            print('options.exclude: {}'.format(options.exclude))
+            print('options.extensions: {}'.format(options.extensions))
+            print('options.languages: {}'.format(options.languages))
             result = lizard.analyze(
                 options.paths,
+                # src_dir,
                 options.exclude,
                 options.working_threads,
                 options.extensions,
                 options.languages)
             printer(result, options, schema, lizard.AllResult)
             lizard.print_extension_results(options.extensions)
-            list(result)
+            result = "".join(list(result))
             if output_file:
                 sys.stdout = original_stdout
                 output_file.close()
 
-            print(result)
+            # lizard output: /usr/src/gmock/src/gmock-spec-builders.cc:330: warning: testing::internal::GTEST_LOCK_EXCLUDED_ has 60 NLOC, 16 CCN, 414 token, 1 PARAM, 106 length
+            print("Lizard result: {}".format(result))
 
         except OSError as ex:
             print("Couldn't find lizard executable! ({})".format(ex))
