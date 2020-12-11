fd84286d4eb7a9731c468e9fcdbd3f21d62035f4
Thomas Denewiler <tdenewiler@gmail.com>
2020-12-10 18:02:50 -0800

    Add support to ROS discovery plugin for ROS2 Python-only packages that do not contain CMakeLists.txt. Second attempt that fixed some bugs from first attempt.

--- statick_tool/plugins/discovery/ros_discovery_plugin.py
+++ statick_tool/plugins/discovery/ros_discovery_plugin.py
@@ -1,6 +1,9 @@
 """Discovery plugin to find ROS packages."""
 import os
-from typing import Optional
+from functools import reduce
+from typing import Any, Dict, Optional, Union
+
+import xmltodict
 
 from statick_tool.discovery_plugin import DiscoveryPlugin
 from statick_tool.exceptions import Exceptions
@@ -13,6 +16,24 @@ class RosDiscoveryPlugin(DiscoveryPlugin):
     def get_name(self) -> str:
         """Get name of discovery type."""
         return "ros"
+
+    @classmethod
+    def deep_get(
+        cls,
+        dictionary: Union[str, Dict[Any, str]],
+        keys: str,
+        default: Optional[str] = None,
+    ) -> Any:
+        """Safe way to check for a value in a nested dict.
+
+        Copied from:
+        https://stackoverflow.com/questions/25833613/python-safe-method-to-get-value-of-nested-dictionary
+        """
+        return reduce(
+            lambda d, key: d.get(key, default) if isinstance(d, dict) else default,
+            keys.split("."),
+            dictionary,
+        )
 
     def scan(
         self, package: Package, level: str, exceptions: Optional[Exceptions] = None
@@ -40,6 +61,20 @@ class RosDiscoveryPlugin(DiscoveryPlugin):
                             package[
                                 "cmake_flags"
                             ] = "-DCMAKE_PREFIX_PATH=" + item.rstrip("/bin")
+        elif os.path.isfile(package_file) and ros_version is not None:
+            with open(package_file) as fconfig:
+                try:
+                    output = xmltodict.parse(fconfig.read())
+                except (
+                    xmltodict.expat.ExpatError,
+                    xmltodict.ParsingInterrupted,
+                ) as exc:
+                    # No valid XML found, so we are not going to find the build type.
+                    package["ros"] = False
+                    print("  Invalid XML in {}: {}".format(package_file, exc))
+                    return
+                if self.deep_get(output, "package.export.build_type") == "ament_python":
+                    print("  Package is ROS{}.".format(ros_version))
+                    package["ros"] = True
         else:
-            print("  Package is not ROS.")
             package["ros"] = False
