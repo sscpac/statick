a5f4766ef3fff71235bb2db56fc15ba4b824870b
Thomas Denewiler <tdenewiler@gmail.com>
2020-12-15 08:13:04 -0800

    Remove try/except for lizard. Make module functions part of the class.

--- statick_tool/plugins/tool/lizard_tool_plugin.py
+++ statick_tool/plugins/tool/lizard_tool_plugin.py
@@ -10,25 +10,12 @@ from statick_tool.issue import Issue
 from statick_tool.package import Package
 from statick_tool.tool_plugin import ToolPlugin
 
-DISABLED_FLAGS = ["-f", "--input_file", "-o", "--output_file", "-Edumpcomments"]
-
-
-def _valid_flag(flag: str) -> bool:
-    """Indicate if passed flag is invalid."""
-    if flag in DISABLED_FLAGS:
-        return False
-    return True
-
-
-def remove_invalid_flags(flag_list: List[str]) -> List[str]:
-    """Filter out all flags in the DISABLED_FLAGS list."""
-    return [x for x in flag_list if _valid_flag(x)]
-
 
 class LizardToolPlugin(ToolPlugin):
     """Apply Lizard tool and gather results.
 
-    NOTE: The `-f/--input_file`, `-o/--output_file`, and `-Edumpcomments` options are unsupported
+    Note: The `-f/--input_file`, `-o/--output_file`, and `-Edumpcomments`
+    options are unsupported.
     """
 
     def get_name(self) -> str:
@@ -40,47 +27,42 @@ class LizardToolPlugin(ToolPlugin):
         if not package.path:
             return []
 
-        # The following is a modification of lizard.py's main() #
+        # The following is a modification of lizard.py's main().
         raw_user_flags = (
             [lizard.__file__] + [package.path] + self.get_user_flags(level)
-        )  # leading lizard file name is required
+        )  # Leading lizard file name is required.
 
-        # Make sure we log warnings
+        # Make sure we log warnings.
         if "-w" not in raw_user_flags:
             raw_user_flags += ["-w"]
 
-        # Make sure unsupported arguments are not included
-        user_flags = remove_invalid_flags(raw_user_flags)
+        # Make sure unsupported arguments are not included.
+        user_flags = self.remove_invalid_flags(raw_user_flags)
 
         options = lizard.parse_args(user_flags)
         printer = options.printer or lizard.print_result
         schema = lizard.OutputScheme(options.extensions)
         schema.patch_for_extensions()
 
-        try:
-            result = lizard.analyze(
-                options.paths,
-                options.exclude,
-                options.working_threads,
-                options.extensions,
-                options.languages,
-            )
-            lizard_output = io.StringIO()
-            with redirect_stdout(lizard_output):
-                printer(result, options, schema, lizard.AllResult)
-            output = lizard_output.getvalue()
-            lizard.print_extension_results(options.extensions)
-            list(result)
+        result = lizard.analyze(
+            options.paths,
+            options.exclude,
+            options.working_threads,
+            options.extensions,
+            options.languages,
+        )
+        lizard_output = io.StringIO()
+        with redirect_stdout(lizard_output):
+            printer(result, options, schema, lizard.AllResult)
+        output = lizard_output.getvalue()
+        lizard.print_extension_results(options.extensions)
 
-            if self.plugin_context:
-                if self.plugin_context.args.show_tool_output:
-                    print("{}".format(output))
-                if self.plugin_context.args.output_directory:
-                    with open(self.get_name() + ".log", "w") as fid:
-                        fid.write(output)
-        except OSError as ex:
-            print("Error occurred while running lizard! ({})".format(ex))
-            return None
+        if self.plugin_context:
+            if self.plugin_context.args.show_tool_output:
+                print("{}".format(output))
+            if self.plugin_context.args.output_directory:
+                with open(self.get_name() + ".log", "w") as fid:
+                    fid.write(output)
 
         issues = self.parse_output(output)
 
@@ -105,3 +87,15 @@ class LizardToolPlugin(ToolPlugin):
                 issues.append(issue)
 
         return issues
+
+    def remove_invalid_flags(self, flag_list: List[str]) -> List[str]:
+        """Filter out all disabled flags."""
+        return [x for x in flag_list if self.valid_flag(x)]
+
+    @classmethod
+    def valid_flag(cls, flag: str) -> bool:
+        """Indicate if passed flag is invalid."""
+        disabled_flags = ["-f", "--input_file", "-o", "--output_file", "-Edumpcomments"]
+        if flag in disabled_flags:
+            return False
+        return True
