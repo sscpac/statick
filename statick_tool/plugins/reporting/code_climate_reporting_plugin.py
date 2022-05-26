f3420083cb170438ebb2e5836aad2fc32f9d3223
Alexander Xydes <alexander.l.xydes.civ@us.navy.mil>
2022-05-25 17:20:31 -0700

    Adding initial code-climate reporter plugin.

---
+++ statick_tool/plugins/reporting/code_climate_reporting_plugin.py
@@ -0,0 +1,120 @@
+"""Prints the Statick reports out to the terminal or file in JSON format."""
+import hashlib
+import json
+import logging
+import os
+from collections import OrderedDict
+from typing import Dict, List, Optional, Tuple
+
+from statick_tool.issue import Issue
+from statick_tool.package import Package
+from statick_tool.reporting_plugin import ReportingPlugin
+
+
+class CodeClimateReportingPlugin(ReportingPlugin):
+    """Prints the Statick reports out to the terminal or file in JSON format."""
+
+    def get_name(self) -> str:
+        """Return the plugin name."""
+        return "code_climate"
+
+    def report(
+        self, package: Package, issues: Dict[str, List[Issue]], level: str
+    ) -> Tuple[Optional[None], bool]:
+        """Go through the issues list and print them in JSON format.
+
+        Args:
+            package (:obj:`Package`): The Package object that was analyzed.
+            issues (:obj:`dict` of :obj:`str` to :obj:`Issue`): The issues
+                found by the Statick analysis, keyed by the tool that found
+                them.
+            level: (:obj:`str`): Name of the level used in the scan.
+        """
+        if not self.plugin_context or not self.plugin_context.config:
+            return None, False
+
+        file_output = False
+        terminal_output = False
+        file_output_str = self.plugin_context.config.get_reporting_config(
+            self.get_name(), level, "files"
+        )
+        if file_output_str and file_output_str.lower() == "true":
+            file_output = True
+        terminal_output_str = self.plugin_context.config.get_reporting_config(
+            self.get_name(), level, "terminal"
+        )
+        if terminal_output_str and terminal_output_str.lower() == "true":
+            terminal_output = True
+
+        all_issues = []
+        for _, value in issues.items():
+            for issue in value:
+                severity = "info"
+                try:
+                    if int(issue.severity) > 0:
+                        severity = "minor"
+                    if int(issue.severity) > 2:
+                        severity = "major"
+                    if int(issue.severity) > 4:
+                        severity = "critical"
+                except ValueError as ex:
+                    logging.warning(
+                        "Invalid severity integer (%s), using default 'info' "
+                        " severity. Error = %s",
+                        issue.severity,
+                        ex,
+                    )
+                issue_dict = OrderedDict()
+                issue_dict["type"] = "issue"
+                issue_dict["check_name"] = issue.tool
+                issue_dict["severity"] = severity
+                issue_dict["categories"] = []
+
+                # gitlab only uses the description field, so including issue.tool here too
+                description = (
+                    issue.tool + ": " + issue.issue_type + ": " + issue.message
+                )
+                if issue.cert_reference:
+                    description += ", CERT Reference: " + issue.cert_reference
+                    issue_dict["categories"].append("Security")
+                issue_dict["description"] = description
+
+                issue_dict["location"] = OrderedDict()
+                issue_dict["location"]["path"] = issue.filename
+                issue_dict["location"]["lines"] = OrderedDict()
+                issue_dict["location"]["lines"]["begin"] = issue.line_number
+
+                fingerprint = hashlib.md5(json.dumps(issue_dict).encode())
+                issue_dict["fingerprint"] = fingerprint.hexdigest()
+
+                all_issues.append(issue_dict)
+        line = json.dumps(all_issues)
+
+        if file_output:
+            if not self.write_output(package, level, line):
+                return None, False
+
+        if terminal_output:
+            print(line)
+
+        return None, True
+
+    def write_output(self, package: Package, level: str, line: str) -> bool:
+        """Write JSON output to a file."""
+        if not self.plugin_context:
+            return False
+
+        output_dir = os.path.join(
+            self.plugin_context.args.output_directory, package.name + "-" + level
+        )
+        if not os.path.exists(output_dir):
+            os.mkdir(output_dir)
+        if not os.path.isdir(output_dir):
+            logging.error("Unable to create output directory at %s!", output_dir)
+            return False
+        output_file = os.path.join(output_dir, package.name + "-" + level + ".json")
+        logging.info("Writing output to %s", output_file)
+        with open(output_file, "w", encoding="utf8") as out:
+            out.write(line)
+
+        return True
---
+++ tests/plugins/reporting/code_climate_reporting_plugin/test_code_climate_reporting_plugin.py
@@ -0,0 +1,130 @@
+"""Unit tests for the JSON reporting plugin."""
+import argparse
+import os
+
+from yapsy.PluginManager import PluginManager
+
+import statick_tool
+from statick_tool.config import Config
+from statick_tool.issue import Issue
+from statick_tool.package import Package
+from statick_tool.plugin_context import PluginContext
+from statick_tool.plugins.reporting.code_climate_reporting_plugin import (
+    CodeClimateReportingPlugin,
+)
+from statick_tool.reporting_plugin import ReportingPlugin
+from statick_tool.resources import Resources
+
+try:
+    from tempfile import TemporaryDirectory
+except:  # pylint: disable=bare-except # noqa: E722 # NOLINT
+    from backports.tempfile import (  # pylint: disable=wrong-import-order
+        TemporaryDirectory,
+    )
+
+
+def setup_code_climate_reporting_plugin(file_path, use_plugin_context=True):
+    """Create an instance of the file writer plugin."""
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("output_directory")
+
+    resources = Resources([os.path.join(os.path.dirname(__file__), "config")])
+    config = Config(resources.get_file("config.yaml"))
+    plugin = CodeClimateReportingPlugin()
+    if use_plugin_context:
+        plugin_context = PluginContext(
+            arg_parser.parse_args([file_path]), resources, config
+        )
+        plugin.set_plugin_context(plugin_context)
+    return plugin
+
+
+def test_code_climate_reporting_plugin_found():
+    """Test that the plugin manager finds the plugin."""
+    manager = PluginManager()
+    # Get the path to statick_tool/__init__.py, get the directory part, and
+    # add 'plugins' to that to get the standard plugins dir
+    manager.setPluginPlaces(
+        [os.path.join(os.path.dirname(statick_tool.__file__), "plugins")]
+    )
+    manager.setCategoriesFilter(
+        {
+            "Reporting": ReportingPlugin,
+        }
+    )
+    manager.collectPlugins()
+    # Verify that a plugin's get_name() function returns "code_climate"
+    assert any(
+        plugin_info.plugin_object.get_name() == "code_climate"
+        for plugin_info in manager.getPluginsOfCategory("Reporting")
+    )
+    # While we're at it, verify that a plugin is named Code Climate Discovery Plugin
+    assert any(
+        plugin_info.name == "Code Climate Reporting Plugin"
+        for plugin_info in manager.getPluginsOfCategory("Reporting")
+    )
+
+
+def test_code_climate_reporting_plugin_report_cert_reference():
+    """Test the output of the reporting plugin where the issue has a CERT reference."""
+    with TemporaryDirectory() as tmp_dir:
+        plugin = setup_code_climate_reporting_plugin(tmp_dir)
+        package = Package(
+            "valid_package", os.path.join(os.path.dirname(__file__), "valid_package")
+        )
+        issues = {
+            "tool_a": [
+                Issue("test.txt", 1, "tool_a", "type", "1", "This is a test", "CERT")
+            ]
+        }
+        _, success = plugin.report(package, issues, "level")
+        assert success
+
+
+def test_code_climate_reporting_plugin_report_no_plugin_context():
+    """Test the output of the reporting plugin without plugin context."""
+    with TemporaryDirectory() as tmp_dir:
+        plugin = setup_code_climate_reporting_plugin(tmp_dir, False)
+        package = Package(
+            "valid_package", os.path.join(os.path.dirname(__file__), "valid_package")
+        )
+        issues = {
+            "tool_a": [
+                Issue("test.txt", 1, "tool_a", "type", "1", "This is a test", None)
+            ]
+        }
+        _, success = plugin.report(package, issues, "level")
+        assert not success
+
+
+def test_code_climate_reporting_plugin_report_fileexists():
+    """Test the output of the reporting plugin if there's a file where the output dir should go."""
+    with TemporaryDirectory() as tmp_dir:
+        plugin = setup_code_climate_reporting_plugin(tmp_dir)
+        package = Package(
+            "valid_package", os.path.join(os.path.dirname(__file__), "valid_package")
+        )
+        # Makes a file where we expect a dir
+        open(os.path.join(tmp_dir, package.name + "-" + "level"), "w").close()
+        issues = {
+            "tool_a": [
+                Issue("test.txt", 1, "tool_a", "type", 1, "This is a test", None)
+            ]
+        }
+        _, success = plugin.report(package, issues, "level")
+        assert os.path.isfile(
+            os.path.join(
+                plugin.plugin_context.args.output_directory, "valid_package-level"
+            )
+        )
+    assert not success
+
+
+def test_code_climate_reporting_plugin_write_output_no_plugin_context():
+    """Test the write output method of the reporting plugin without plugin context."""
+    with TemporaryDirectory() as tmp_dir:
+        plugin = setup_code_climate_reporting_plugin(tmp_dir, False)
+        package = Package(
+            "valid_package", os.path.join(os.path.dirname(__file__), "valid_package")
+        )
+        assert not plugin.write_output(package, "level", "line")
