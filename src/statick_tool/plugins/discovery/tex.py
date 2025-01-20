5778da79cc00c65a57c85f737db1cd15e3eddae5
Thomas Denewiler <tdenewiler@gmail.com>
2025-01-20 12:59:38 -0800

    Use setuptools for plugin mechanism. (#85)
    * Switch from setup.py to pyproject.toml, update unit tests, update github workflows.
    * Add apt install instructions.
    * Switch from os module to pathlib/Path for checking/removing test artifact files.
    * Update action versions.
    * Drop Python 3.8.
    * Add Python 3.13.
    * Add ubuntu-22.04 to CI.

---
+++ src/statick_tool/plugins/discovery/tex.py
@@ -0,0 +1,47 @@
+"""Discover TeX files to analyze."""
+import logging
+from collections import OrderedDict
+from typing import List, Tuple
+
+from statick_tool.discovery_plugin import DiscoveryPlugin
+from statick_tool.exceptions import Exceptions
+from statick_tool.package import Package
+
+
+class TexDiscoveryPlugin(DiscoveryPlugin):  # type: ignore
+    """Discover TeX files to analyze."""
+
+    def get_name(self) -> str:
+        """Get name of discovery type."""
+        return "tex"
+
+    def scan(self, package: Package, level: str, exceptions: Exceptions = None) -> None:
+        """Scan package looking for TeX files."""
+        tex_files: List[str] = []
+        tex_extensions: Tuple[str, str] = (".tex", ".bib")
+        tex_ignore_extensions = (".sty", ".log", ".cls")
+        tex_output = ["latex document", "bibtex text file", "latex 2e document"]
+
+        self.find_files(package)
+
+        for file_dict in package.files.values():
+            if file_dict["name"].endswith(tex_extensions):
+                tex_files.append(file_dict["path"])
+
+            if any(
+                item in file_dict["file_cmd_out"] for item in tex_output
+            ) and not file_dict["name"].endswith(tex_ignore_extensions):
+                tex_files.append(file_dict["path"])
+
+        tex_files = list(OrderedDict.fromkeys(tex_files))
+
+        logging.info("  %d TeX files found.", len(tex_files))
+        if exceptions:
+            original_file_count: int = len(tex_files)
+            tex_files = exceptions.filter_file_exceptions_early(package, tex_files)
+            if original_file_count > len(tex_files):
+                logging.info(
+                    "  After filtering, %d TeX files will be scanned.", len(tex_files)
+                )
+
+        package["tex"] = tex_files
---
+++ src/statick_tool/plugins/tool/lacheck.py
@@ -0,0 +1,90 @@
+"""Apply lacheck tool and gather results."""
+import logging
+import re
+import subprocess
+from typing import List, Match, Optional, Pattern
+
+from statick_tool.issue import Issue
+from statick_tool.package import Package
+from statick_tool.tool_plugin import ToolPlugin
+
+
+class LacheckToolPlugin(ToolPlugin):  # type: ignore
+    """Apply lacheck tool and gather results."""
+
+    def get_name(self) -> str:
+        """Get name of tool."""
+        return "lacheck"
+
+    def get_file_types(self) -> List[str]:
+        """Return a list of file types the plugin can scan."""
+        return ["tex"]
+
+    # pylint: disable=too-many-locals
+    def process_files(
+        self, package: Package, level: str, files: List[str], user_flags: List[str]
+    ) -> Optional[List[str]]:
+        """Run tool and gather output."""
+        flags: List[str] = []
+        flags += user_flags
+
+        total_output: List[str] = []
+
+        tool_bin: str = "lacheck"
+        try:
+            subproc_args: List[str] = [tool_bin] + flags + files
+            output = subprocess.check_output(
+                subproc_args, stderr=subprocess.STDOUT, universal_newlines=True
+            )
+
+        except subprocess.CalledProcessError as ex:
+            # Return code 1 just means "found problems"
+            if ex.returncode != 1:
+                logging.warning("Problem %d", ex.returncode)
+                logging.warning("%s exception: %s", self.get_name(), ex.output)
+                return None
+            output = ex.output
+
+        except OSError as ex:
+            logging.warning("Couldn't find %s! (%s)", tool_bin, ex)
+            return None
+
+        logging.debug("%s", output)
+
+        total_output.append(output)
+
+        return total_output
+
+    def parse_output(
+        self, total_output: List[str], package: Optional[Package] = None
+    ) -> List[Issue]:
+        """Parse tool output and report issues."""
+        tool_re: str = r"(.+)\s(.+)\s(\d+):\s(.+)"
+        parse: Pattern[str] = re.compile(tool_re)
+        issues: List[Issue] = []
+        filename: str = ""
+        line_number: str = "0"
+        issue_type: str = ""
+        message: str = ""
+
+        for output in total_output:
+            for line in output.splitlines():
+                match: Optional[Match[str]] = parse.match(line)
+                if match:
+                    filename = match.group(1)[1:-2]
+                    issue_type = "lacheck"
+                    line_number = match.group(3)
+                    message = match.group(4)
+                    issues.append(
+                        Issue(
+                            filename,
+                            line_number,
+                            self.get_name(),
+                            issue_type,
+                            "3",
+                            message,
+                            None,
+                        )
+                    )
+
+        return issues
