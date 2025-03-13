bc4ca6fcbeb17513408e52a39d1192c222984b15
Alexander Xydes <alexander.l.xydes.civ@us.navy.mil>
2025-03-13 00:31:01 +0000

    Removing unused code

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -3,12 +3,6 @@
 REPO_NAME=statick
 HOME=/opt
 
-# echo "Installing 'niwc' command..."
-# The `ln -s` is what allows the commands to be run in Bash just by using the command name.
-# So instead of running $HOME/$REPO_NAME/.devcontainer/scripts/niwc.sh,
-# the user can just run niwc from anywhere in the DevContainer.
-# ln -s $HOME/$REPO_NAME/.devcontainer/scripts/niwc.sh ~/.local/bin/niwc
-
 # Uncomment this line if necessary.
 # git config --global --add safe.directory $HOME/$REPO_NAME
 
