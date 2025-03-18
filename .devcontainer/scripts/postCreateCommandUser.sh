b0c9d166341d8af92de0a623cf1c4c0bcee831af
Alexander Xydes <alexander.l.xydes.civ@us.navy.mil>
2025-03-18 22:16:42 +0000

    Updating .bashrc modifications to hard-code path.

--- .devcontainer/scripts/postCreateCommandUser.sh
+++ .devcontainer/scripts/postCreateCommandUser.sh
@@ -7,14 +7,18 @@ uv venv /opt/venv
 # shellcheck disable=SC2102
 uv pip install -e .[docs,test]
 
+echo "Update .bashrc ..."
 echo "" >> ~/.bashrc
-echo "# Activate statick virtual environment." >> ~/.bashrc
+echo "# Activate statick virtual environment." >> /root/.bashrc
 # shellcheck disable=SC2016
-echo ". /opt/venv/bin/activate" >> "${HOME}"/.bashrc
+echo ". /opt/venv/bin/activate" >> /root/.bashrc
+echo /root/.bashrc
+cat /root/.bashrc
 
 echo "" >> ~/.bashrc
-echo "alias pip='uv pip'" >> "${HOME}"/.bashrc
-echo "alias self-check='statick . --level self_check --check --output-dir /tmp/statick-output --log info'" >> "${HOME}"/.bashrc
+echo "alias pip='uv pip'" >> /root/.bashrc
+echo "alias pip='uv pip'" >> /root/.bashrc
+echo "alias self-check='statick . --level self_check --check --output-dir /tmp/statick-output --log info'" >> /root/.bashrc
 
 echo ""
 echo "Setup complete. Development environment ready to go!!"
