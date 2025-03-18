e964c30f09e21d24f617d359cc393fb43168ff2e
Alexander Xydes <alexander.l.xydes.civ@us.navy.mil>
2025-03-18 22:38:00 +0000

    Removing debug/extra lines from script. Adding dev script with common commands.

--- .devcontainer/scripts/postCreateCommand.sh
+++ .devcontainer/scripts/postCreateCommand.sh
@@ -3,6 +3,12 @@
 REPO_NAME=statick
 HOME=/opt
 
+echo "Installing 'dev' command..."
+# The `ln -s` is what allows the commands to be run in Bash just by using the command name.
+# So instead of running $HOME/$REPO_NAME/.devcontainer/scripts/dev.sh,
+# the user can just run dev from anywhere in the DevContainer.
+ln -s $HOME/$REPO_NAME/.devcontainer/scripts/dev.sh /usr/local/bin/dev
+
 # Uncomment this line if necessary.
 # git config --global --add safe.directory $HOME/$REPO_NAME
 
--- .devcontainer/scripts/postCreateCommandUser.sh
+++ .devcontainer/scripts/postCreateCommandUser.sh
@@ -10,15 +10,12 @@ uv pip install -e .[docs,test]
 echo "Update .bashrc ..."
 echo "" >> ~/.bashrc
 echo "# Activate statick virtual environment." >> /root/.bashrc
-# shellcheck disable=SC2016
 echo ". /opt/venv/bin/activate" >> /root/.bashrc
-echo /root/.bashrc
-cat /root/.bashrc
 
 echo "" >> ~/.bashrc
 echo "alias pip='uv pip'" >> /root/.bashrc
-echo "alias pip='uv pip'" >> /root/.bashrc
-echo "alias self-check='statick . --level self_check --check --output-dir /tmp/statick-output --log info'" >> /root/.bashrc
 
+dev --help
 echo ""
 echo "Setup complete. Development environment ready to go!!"
+echo "Use the command 'dev --help' to get started."
